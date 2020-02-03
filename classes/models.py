fe9f805c7c433b37ece7ff1fc370a957a9958722
lailalelouch <39729644+lailalelouch@users.noreply.github.com>
2020-02-03 19:50:13 +0300

    Update models.py

--- classes/models.py
+++ classes/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.urls import reverse
 
 class Classroom(models.Model):
-	name = models.CharField(max_length=120)
 	subject = models.CharField(max_length=120)
+	grade = models.IntegerField()
 	year = models.IntegerField()
 
 	def get_absolute_url(self):
