897b8af8a669a4c511404dbedd6d5e580ce1827a
khalid almuhawis <khalidalmuhawis0@gmail.com>
2020-09-08 10:28:23 +0300

    added teacher to classrooms, student model

--- classes/models.py
+++ classes/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.urls import reverse
 
 class Classroom(models.Model):
+	teacher = models.ForeignKey(User, on_delete=models.CASCADE)
 	subject = models.CharField(max_length=120)
 	grade = models.IntegerField()
 	year = models.IntegerField()
 
 	def get_absolute_url(self):
 		return reverse('classroom-detail', kwargs={'classroom_id':self.id})
+class Student(models.Model):
+	male = "M"
+	female = "F"
+	name = models.CharField(max_length=120)
+	dob = models.DateField()
+	gender = models.CharField(max_length=1, choices=[(male,"male"),(female,"female")])
+	exam_grade = models.IntegerField()
+	classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
--- classes/views.py
+++ classes/views.py
@@ -64,11 +64,15 @@ def classroom_detail(request, classroom_id):
 
 
 def classroom_create(request):
+    if not request.user.is_authenticated:
+        return redirect('signin')
     form = ClassroomForm()
     if request.method == "POST":
         form = ClassroomForm(request.POST, request.FILES or None)
         if form.is_valid():
-            form.save()
+            item = form.save(commit=False)
+            item.teacher = request.user
+            item.save()
             messages.success(request, "Successfully Created!")
             return redirect('classroom-list')
         print(form.errors)
